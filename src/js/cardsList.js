b58de126d146dafdc77e5fe1e6cd71416b999830
Alina Kasianova <avkasyanova@gmail.com>
2021-08-12 13:24:13 +0300

    Update cardsList.js

--- src/js/cardsList.js
+++ src/js/cardsList.js
@@ -2,16 +2,15 @@
 import cardsMarkup from '../templates/cardsMarkup.hbs';
 import menu from '../menu.json';
 
-console.log(cardsMarkup);
 
 const cardsListRefs = document.querySelector('.js-menu');
 const cardsRef = cardsMarkup(menu);
 
-createCardsMarkup(menu);
+// createCardsMarkup(menu);
 
 cardsListRefs.insertAdjacentHTML('beforeend', cardsRef);
 
-function createCardsMarkup(menu) {
-    return menu.map(cardsMarkup).join('');
-}
+// function createCardsMarkup() {
+//     return menu.map(cardsMarkup).join('');
+// }
 
